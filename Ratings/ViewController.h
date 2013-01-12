0a6731a9170f090b8046e3fcbb6d9f33e089d9eb
Drew Stinnett <dstinnett@macretina-1244.gisinc.local>
2013-01-11 18:48:22 -0800

    Initial Commit

---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by Drew Stinnett on 1/11/13.
+//  Copyright (c) 2013 T3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
